b7627006a6fb303f988b86c991bd343e3aa8fced
Garnet638 <garnet638@gmail.com>
2017-07-31 19:04:53 -0400

    Initial commit, just doing this to set up the repo

---
+++ index.js
@@ -0,0 +1,24 @@
+var electron = require('electron');
+var path = require('path');
+var url = require('url');
+
+var vKeyWindow = null;
+
+electron.app.once('ready', function () {
+	vKeyWindow = new electron.BrowserWindow({
+    width: 1024,
+		height: 576,
+		resizable: false,
+		show: false,
+    frame: false
+	});
+	vKeyWindow.loadURL(url.format({
+    	pathname: path.join(__dirname, 'app', 'index.html'),
+    	protocol: 'file:',
+    	slashes: true
+	}));
+	vKeyWindow.once('ready-to-show', function () {
+		// vKeyWindow.setMenu(null);
+		vKeyWindow.show();
+	});
+});
